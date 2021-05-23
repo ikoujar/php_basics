b098096cbe91dcacf9d6e5e9782d97b02de66f76
Ibrahim Koujar <ibrahimkoujar92@gmail.com>
2021-05-23 13:06:31 +0300

    debug

--- config.php
+++ config.php
@@ -3,16 +3,19 @@
 $dotenv = \Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
-return [
-    'app' => [
-        'home_url' => $_ENV['APP_URL']
-    ],
-    'database' => [
-        'dsn' => $_ENV['DB_DSN'],
-        'connection' => $_ENV['DB_CONNECTION'] ?? 'mysql',
-        'host' => $_ENV['DB_HOST'],
-        'name' => $_ENV['DB_NAME'],
-        'user' => $_ENV['DB_USER'],
-        'password' => $_ENV['DB_PASSWORD']
-    ]
-];
+print_r($_ENV);
+exit;
+
+//return [
+//    'app' => [
+//        'home_url' => $_ENV['APP_URL']
+//    ],
+//    'database' => [
+//        'dsn' => $_ENV['DB_DSN'],
+//        'connection' => $_ENV['DB_CONNECTION'] ?? 'mysql',
+//        'host' => $_ENV['DB_HOST'],
+//        'name' => $_ENV['DB_NAME'],
+//        'user' => $_ENV['DB_USER'],
+//        'password' => $_ENV['DB_PASSWORD']
+//    ]
+//];
