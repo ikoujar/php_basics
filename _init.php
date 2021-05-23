057c7d763cdccd4863feee2e4118a7fb354f84d4
Ibrahim Koujar <ibrahimkoujar92@gmail.com>
2021-05-23 12:59:43 +0300

    debug

--- _init.php
+++ _init.php
@@ -13,6 +13,8 @@ App::set('config', require 'config.php');
 //$log = new Logger('PHP_BASICS');
 //$log->pushHandler(new StreamHandler('queries.log', Logger::INFO));
 
-QueryBuilder::make(
-    DBConnection::make(App::get('config')['database'])
-);
+//QueryBuilder::make(
+//    DBConnection::make(App::get('config')['database'])
+//);
+
+print_r(App::get('config')['database']);
