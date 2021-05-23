499f98ee02378aef12311652096e4458eeef79b9
Ibrahim Koujar <ibrahimkoujar92@gmail.com>
2021-05-23 13:03:01 +0300

    debug

--- _init.php
+++ _init.php
@@ -1,20 +1,20 @@
 <?php
 
-//use App\App;
-//use App\Database\DBConnection;
-//use App\Database\QueryBuilder;
-//use Monolog\Logger;
-//use Monolog\Handler\StreamHandler;
-//
-//require 'vendor/autoload.php';
-//
-//App::set('config', require 'config.php');
+use App\App;
+use App\Database\DBConnection;
+use App\Database\QueryBuilder;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+
+require 'vendor/autoload.php';
+
+App::set('config', require 'config.php');
 
 //$log = new Logger('PHP_BASICS');
 //$log->pushHandler(new StreamHandler('queries.log', Logger::INFO));
 
-//QueryBuilder::make(
-//    DBConnection::make(App::get('config')['database'])
-//);
+QueryBuilder::make(
+    DBConnection::make(App::get('config')['database'])
+);
 
-print_r('xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx');
+print_r(App::get('config')['database']);
